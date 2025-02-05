9b268b0cbfbc1c863733207f1d451a4ad59ed7c0
seabye0 <148069700+seabye0@users.noreply.github.com>
2025-02-05 16:59:55 +0200

    Update main.java

--- yes/main.java
+++ yes/main.java
@@ -1,37 +1,38 @@
-import greenfoot.*;  // Importing Greenfoot library
+import greenfoot.*; 
 
-// Main world class that extends World, creating the main menu for the game
+// Clasa "main" extinde World
+// acesata este meniul principal al jocului
 public class main extends World
 {
-    // Constructor for the main world, initializes the world with size 600x400 and cell size 1
+    // construim meniul principal
+    // inițializaăm lumea cu dimensiunea 600x400 și celule de 1x1 pixel
     public main()
     {    
         super(600, 400, 1);
-        prepare();  // Prepare the world (add the buttons)
+        prepare();  // apelează metoda pentru a adăuga butoanele
     }
     
-    // Method to add objects (buttons) to the world
     public void prepare()
     {
-        // Create and add the "Play" button
-        play b = new play();
-        GreenfootImage c = b.getImage();  // Get the image for the play button
-        c.scale(90, 30);  // Scale the image to 90x30 pixels
-        b.setImage(c);  // Set the resized image to the button
-        addObject(b, 110, 210);  // Add the button to the world at the specified coordinates (110, 210)
+        // Crearea și adăugarea butonului "Play"
+        play b = new play();  // Creează un nou obiect de tip "play"
+        GreenfootImage c = b.getImage();  // Obține imaginea butonului
+        c.scale(90, 30);  // Redimensionează imaginea la 90x30 pixeli
+        b.setImage(c);  // Aplică imaginea redimensionată
+        addObject(b, 110, 210);  // Plasează butonul în lume la coordonatele (110, 210)
         
-        // Create and add the "How to Play" button
-        how a = new how();
-        c = a.getImage();  // Get the image for the how-to-play button
-        c.scale(90, 30);  // Scale the image to 90x30 pixels
-        a.setImage(c);  // Set the resized image to the button
-        addObject(a, 110, 250);  // Add the button to the world at the specified coordinates (110, 250)
+        // la fel ca mai sus pentru butonul  "How to Play"
+        how a = new how();  
+        c = a.getImage();  
+        c.scale(90, 30);  
+        a.setImage(c);  
+        addObject(a, 110, 250); 
         
-        // Create and add the "Quit" button
-        quit d = new quit();
-        c = d.getImage();  // Get the image for the quit button
-        c.scale(90, 30);  // Scale the image to 90x30 pixels
-        d.setImage(c);  // Set the resized image to the button
-        addObject(d, 110, 290);  // Add the button to the world at the specified coordinates (110, 290)
-    }
+        // la fel și pentru butonul "Quit"
+        quit d = new quit();  
+        c = d.getImage();  
+        c.scale(90, 30);  
+        d.setImage(c);  
+        addObject(d, 110, 290); 
+    }
 }
